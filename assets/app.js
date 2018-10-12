6c5bbe840d83204e434a20aac36464025784feb3
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-10-11 23:24:57 -0500

    linked css and js, liked jQuery and Firebase database

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+<script>
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBtbAFzXc1SvrsxmpUw4UyERs1uz9r6UAs",
+    authDomain: "train-schedule-167d1.firebaseapp.com",
+    databaseURL: "https://train-schedule-167d1.firebaseio.com",
+    projectId: "train-schedule-167d1",
+    storageBucket: "train-schedule-167d1.appspot.com",
+    messagingSenderId: "364153720440"
+  };
+  firebase.initializeApp(config);
+</script>
