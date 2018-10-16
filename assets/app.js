27c18e6a64754a1c02b1e058060c615cee7bb098
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-10-16 16:01:15 -0500

    added firebase with keys, working submit button

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,60 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBtbAFzXc1SvrsxmpUw4UyERs1uz9r6UAs",
-    authDomain: "train-schedule-167d1.firebaseapp.com",
-    databaseURL: "https://train-schedule-167d1.firebaseio.com",
-    projectId: "train-schedule-167d1",
-    storageBucket: "train-schedule-167d1.appspot.com",
-    messagingSenderId: "364153720440"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyBtbAFzXc1SvrsxmpUw4UyERs1uz9r6UAs",
+  authDomain: "train-schedule-167d1.firebaseapp.com",
+  databaseURL: "https://train-schedule-167d1.firebaseio.com",
+  projectId: "train-schedule-167d1",
+  storageBucket: "train-schedule-167d1.appspot.com",
+  messagingSenderId: "364153720440"
+};
+firebase.initializeApp(config);
 
-  // Connect to database
+// Connect to database
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  // Take user input values and update Firebase
-  $("#submit").click(function (){
-    var userTrainName = $("")
-  })
+// var userTrainName = $("#trainname-input")
+//   .val()
+//   .trim();
+// var userDestination = $("#destination-input")
+//   .val()
+//   .trim();
+// var userFirstTrain = $("#firsttime-input")
+//   .val()
+//   .trim();
+// var userFrequency = $("#frequency-input")
+//   .val()
+//   .trim();
 
-  // When value in database changes, update page
+// Take user input values and update Firebase
+$("#submit").on("click", function() {
+  event.preventDefault();
+  var userTrainName = $("#trainname-input")
+    .val()
+    .trim();
+  var userDestination = $("#destination-input")
+    .val()
+    .trim();
+  var userFirstTrain = $("#firsttime-input")
+    .val()
+    .trim();
+  var userFrequency = $("#frequency-input")
+    .val()
+    .trim();
+  console.log(userTrainName, userDestination, userFirstTrain, userFrequency);
 
-  database.ref().on("value", function(snapshot) {
-    console.log(snapshot.val());
+  database.ref().set({
+    savedTrainName: userTrainName,
+    savedDestination: userDestination,
+    savedFirstTrain: userFirstTrain,
+    savedFrequency: userFrequency
   });
 
+  console.log(userTrainName, userDestination, userFirstTrain, userFrequency);
+});
 
+// When value in database changes, update page display
+
+database.ref().on("value", function(snapshot) {
+  console.log(snapshot.val());
+});
